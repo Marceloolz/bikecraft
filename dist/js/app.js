b8f619ace959f4f34842e4ca78a7b030f07e22dc
Marcelo Gomes Ferreira <mgomes@marlin.net>
2020-07-23 19:41:39 -0300

    Header Pronto

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
     AOS.init();
+
+    //Menu Mobile
+     //Abre
+     $('.Header_Container_MenuMobileIcone').click(function(){
+        $('.Header_Container_Menu').addClass('__is-mobile');
+        $('.Container_Menu_Close').addClass('__is-active');
+    });
+
+    //Fechar
+    $('.Container_Menu_Close').click(function(){
+        $('.Header_Container_Menu').removeClass('__is-mobile');
+        $('.Container_Menu_Close').removeClass('__is-active');
+    });
+   
 })
