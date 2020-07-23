dd5980b1aafa697913885348c831f5131af4a30f
Marcelo Gomes Ferreira <mgomes@marlin.net>
2020-07-23 20:12:09 -0300

    Superbanner em andamento

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php include('includes/header.php'); ?>
+<div class="Site_Superbanner __is-banner1">
+    <div class="Site_Superbanner_Container">
+        <div class="Superbanner_Container_Titulo">
+            Bicicletas Feitas a Mão
+        </div>
+        <div class="Superbanner_Container_Subtitulo">
+            "não tenha nada em sua casa que você
+            não considere útil ou acredita ser bonito"
+        </div>
+        <div class="Superbanner_Container_SubtituloAutor">
+            WILLIAM MORRIS
+        </div>
+        <div class="Superbanner_Container_Button Formgroup">
+            <div class="Container_Button_Content Formgroup_Button">
+                <button class="Formgroup_Button_Item __is-outline">
+                    Orçamento
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
+
 </html>
