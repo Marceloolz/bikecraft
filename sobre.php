05e9af1fa52cdd95caab5e06dfacb0219382bdaf
Marceloolz <marcelooferreira@outlook.com.br>
2020-07-30 22:52:24 -0300

    Pagina de sobre no inicio

---
+++ sobre.php
@@ -0,0 +1,53 @@
+<?php include('includes/header.php'); ?>
+
+<div class="Site Sobre">
+    <div class="Component_Superbanner_Internas __is-PageSobre">
+        <div class="Superbanner_Internas_Container">
+            <div class="Internas_Container_Titulo">
+                Sobre
+            </div>
+            <div class="Internas_Container_Subtitulo">
+                conheça mais sobre a bikcraft
+            </div>
+        </div>
+    </div>
+
+    <div class="Sobre_Principal">
+        <div class="Sobre_Principal_BlocoInicial">
+            <div class="Principal_BlocoInicial_RowOne">
+                <div class="BlocoInicial_RowOne_Item">
+                    <div class="RowOne_Item_Content ComponentHeader">
+                        <div class="ComponentHeader_TituloH4 __is-left">
+                            História, Missão e Visão
+                        </div>
+                        <div class="Item_Content_Texto">
+                            <p>O verdadeiro segredo da felicidade está em ter um genuíno interesse por todos os detalhes
+                                da vida cotidiana. Interesse por todos os detalhes da vida cotidiana.</p>
+
+                            <p>O verdadeiro segredo da felicidade está em ter um genuíno. interesse por todos os
+                                detalhes da vida cotidiana. Interesse por todos os detalhes da vida cotidiana.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="BlocoInicial_RowOne_Item">
+                    <div class="RowOne_Item_Content ComponentHeader">
+                        <div class="ComponentHeader_TituloH4 __is-left">
+                            Valores
+                        </div>
+                        <div class="Item_Content_Texto">
+                            - Qualidade no processo com</br>
+                            - Foco no cliente sem perder a</br>
+                            - Diversão, preservando a</br>
+                            - Natureza com sustentabilidade</br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
+</body>
+
+</html>
